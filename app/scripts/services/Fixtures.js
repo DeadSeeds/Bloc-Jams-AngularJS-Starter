c03a8017f9a519d817d6c307df9c2800cf49bb13
Joshua Brown <xxdeadseeds@gmail.com>
2017-09-19 15:22:49 -0700

    checkpoint 6 complete

---
+++ app/scripts/services/Fixtures.js
@@ -0,0 +1,65 @@
+(function() {
+  function Fixtures() {
+    var Fixtures = {};
+
+    var albumPicasso = {
+      title: 'The Colors',
+      artist: 'Pablo Picasso',
+      label: 'Cubism',
+      year: '1881',
+      albumArtUrl: '/assets/images/album_covers/01.png',
+      songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' }
+      ]
+    };
+
+    var albumMarconi = {
+      title: 'The Telephone',
+      artist: 'Guglielmo Marconi',
+      label: 'EM',
+      year: '1901',
+      albumArtUrl: '/assets/images/album_covers/20.png',
+      songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }
+      ]
+    };
+
+    var albumCables = {
+      title: 'Framing Defeat For the Critical Eye',
+      artist: 'Cables & Arms',
+      label: 'Wiretap Records',
+      year: '2016',
+      albumArtUrl: '/assets/images/album_covers/Cables_cover.jpg',
+      songs: [
+        { title: 'Loss of Gain', duration: '191.00', audioUrl: 'assets/music/01\ Loss\ Of\ Gain' },
+        { title: 'Hang the Moon', duration: '179.01', audioUrl: 'assets/music/02\ Hang\ the\ Moon'},
+        { title: 'A Lasting Mark', duration: '259.00', audioUrl: 'assets/music/03\ A\ Lasting\ Mark'},
+        { title: 'Wisdom Teeth', duration: '216.00', audioUrl: 'assets/music/04\ Wisdom\ Teeth'},
+        { title: 'Recurring State', duration: '76.00', audioUrl: 'assets/music/05\ Recurring\ State'},
+        { title: 'Kill Yourself', duration: '156.00', audioUrl: 'assets/music/06\ Kill\ Yourself'},
+        { title: 'Fool Me Twice', duration: '185.00', audioUrl: 'assets/music/07\ Fool\ Me\ Twice'},
+        { title: 'Strongarm', duration: '259.00', audioUrl: 'assets/music/08\ Strongarm'},
+        { title: 'Youll Be Sorry', duration: '216.00', audioUrl: 'assets/music/09\ Youll\ Be\ Sorry'},
+        { title: 'Gift Horse', duration: '194.00', audioUrl: 'assets/music/010\ Gift\ Horse'}
+      ]
+    };
+
+    Fixtures.getAlbum = function() {
+      return albumCables;
+    };
+    
+    return Fixtures;
+  }
+
+  angular
+    .module('blocJams')
+    .factory('Fixtures', Fixtures);
+})();
