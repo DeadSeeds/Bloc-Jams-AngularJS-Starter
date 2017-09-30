236aac98dd8d93203251205ba919ca8f8372136b
Joshua Brown <xxdeadseeds@gmail.com>
2017-09-30 11:43:21 -0700

    checkpoint 10 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
       var currentAlbum = Fixtures.getAlbum();
       var SongPlayer = {};
       /*
@@ -27,6 +27,13 @@
           formats: ['mp3'],
           preload: true
         });
+
+        currentBuzzObject.bind('timeupdate', function() {
+          $rootScope.$apply(function() {
+            SongPlayer.currentTime = currentBuzzObject.getTime();
+          });
+        });
+
         SongPlayer.currentSong = song;
       };
 
@@ -57,6 +64,12 @@
       }
 
       SongPlayer.currentSong = null;
+
+      /*
+      * @desc Current playback time (in seconds) of currently playing song
+      * @type {Number}
+      */
+      SongPlayer.currentTime = null;
 
       /*
       * @function SongPlayer.play
@@ -121,10 +134,22 @@
 
       };
 
+      /*
+      * @function setCurrentTime
+      * @desc Set current time (in seconds) of currently playing song
+      * @param {Number} time
+      */
+
+      SongPlayer.setCurrentTime = function(time) {
+        if (currentBuzzObject) {
+          currentBuzzObject.setTime(time);
+        }
+      };
+
       return SongPlayer;
     }
 
     angular
       .module('blocJams')
-      .factory('SongPlayer', ['Fixtures', SongPlayer]);
+      .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
